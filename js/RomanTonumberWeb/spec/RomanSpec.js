6659e8c49ce2733e33e0fd57526296588ec036c3
simplonco <semia.ladjal@gmail.com>
2015-01-18 17:50:17 +0100

    add V, VI, VII

--- js/RomanTonumberWeb/spec/RomanSpec.js
+++ js/RomanTonumberWeb/spec/RomanSpec.js
@@ -10,9 +10,24 @@ describe("getRomanToNumber", function() {
   
   });
 
-   it("II donne 3", function() {
+   it("III donne 3", function() {
 
     expect(getRomanToNumber("III")).toEqual(3);
+   });
+
+   it("V donne 5", function() {
+
+    expect(getRomanToNumber("V")).toEqual(5);
+   });
+
+   it("VI donne 6", function() {
+
+    expect(getRomanToNumber("VI")).toEqual(6);
+   });
+
+   it("VII donne 7", function() {
+
+    expect(getRomanToNumber("VII")).toEqual(7);
    });
  
 });
--- js/RomanTonumberWeb/src/Roman.js
+++ js/RomanTonumberWeb/src/Roman.js
@@ -1,6 +1,15 @@
 function getRomanToNumber(roman) {
 
-	return roman.length
+var chiffre = 0
+	if (roman == "V"){
+		chiffre = 5
+	} else if (roman == "VI"){
+		chiffre = 6
+	}else if (roman == "VII"){
+		chiffre = 7
+	}
+
+		return chiffre || roman.length
 };
 
 
