0521fb21ba036c22bcd1f3a9df605ad0329a28aa
simplonco <semia.ladjal@gmail.com>
2015-01-18 16:28:12 +0100

    variable chiffre

--- js/RomanTonumberWeb/src/Roman.js
+++ js/RomanTonumberWeb/src/Roman.js
@@ -1,16 +1,9 @@
 function getRomanToNumber(roman) {
 
-var chiffre = 0	
-	if (roman.length == 1){
-		chiffre = 1
-
-	}if (roman.length == 2){
-		chiffre = 2	
-
-	}if(roman.length == 3){
-		chiffre = 3
+var chiffre = 0					
+	{
+ 	chiffre = roman.length	
 	}
-
 	return chiffre
 };
 
