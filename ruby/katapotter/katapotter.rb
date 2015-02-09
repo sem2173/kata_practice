a929072ce172d9647bfe418dbd6ef9616d8bde56
Sem <semia.ladjal@gmail.com>
2015-02-09 10:48:27 +0100

    refactor

--- ruby/katapotter/katapotter.rb
+++ ruby/katapotter/katapotter.rb
@@ -1,8 +1,19 @@
 def cart(books)
+
+amount = 0
+
+        # if books == ["book1"]
+            amount = books.length*8
+        # end
+
+        if books == ["book1","book2"]
+            amount = books.length*8*0.95
+        end
+
+        # if books == ["book1","book1"]
+        #     amount = books.length*8       
+        # end
+
+    return amount
     
-    if books == 'book1'
-        8
-    else
-        16
-    end
 end
--- ruby/katapotter/spec/katapotter_spec.rb
+++ ruby/katapotter/spec/katapotter_spec.rb
@@ -3,11 +3,15 @@ require './katapotter'
 describe "katapotter discount" do
 
     it" book1 cost 8" do
-        expect(cart('book1')).to eq(8)
+        expect(cart(["book1"])).to eq(8)
     end
 
     it" two books1 cost 16" do
-        expect(cart('book2')).to eq(16)
+        expect(cart(["book1","book1"])).to eq(16)
+    end
+
+    it" discount 5% for book1 and book2 " do
+        expect(cart(["book1","book2"])).to eq(15.2)
     end
 end
 
